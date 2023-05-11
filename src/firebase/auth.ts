e2a2454697fae2154f07957c863613ce85a93a69
MartenBeech <mortenbech22@hotmail.com>
2023-05-11 09:39:47 +0200

    Fixed AsyncStorage warning

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,15 +1,22 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth/react-native";
 import { app } from "./config";
 import {
   REACT_APP_AUTH_PASSWORD,
   REACT_APP_AUTH_USERNAME_SUFFIX,
-} from "../../.env";
+} from "../../env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface Login {
   username: string;
 }
 
-const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 
 export async function Auth(props: Login) {
   const username = props.username + REACT_APP_AUTH_USERNAME_SUFFIX;
